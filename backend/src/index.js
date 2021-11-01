71be7cc757d8dc25ccb68200a7cccad4c09d8eb5
Ryan Park <ryan.developer99@gmail.com>
2021-11-01 14:23:22 +0900

    add: code for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/youtube", youtubeRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../../client/build/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
