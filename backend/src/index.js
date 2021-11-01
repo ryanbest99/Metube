177f1b0e4ab12fd7c41e124d71ac1641240edf09
Ryan Park <ryan.developer99@gmail.com>
2021-11-01 17:36:57 +0900

    add: postbuild script in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,9 +12,12 @@ app.use("/api/youtube", youtubeRouter);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../../client/build/index.html"));
+    res.sendFile(
+      path.resolve(__dirname + "../../client", "build", "index.html")
+    );
   });
 }
 
